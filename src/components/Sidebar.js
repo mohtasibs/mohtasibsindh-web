d39a07ccd25fa8006ae9d0213068c9a2f24ab88a
Tariq <tariq@wholydigital.com.au>
2024-11-06 10:06:35 +0500

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,13 +8,18 @@ import { SidebarContext } from "@/context/SidebarContext";
 import {
   AiOutlineHome,
   AiOutlineProject,
+
 } from "react-icons/ai";
 import { BsSkipStartCircle, BsLink45Deg } from "react-icons/bs";
 import { BiPhotoAlbum } from "react-icons/bi";
 import { SiOpenapiinitiative } from "react-icons/si";
 import { TiContacts } from "react-icons/ti";
-import { MdKeyboardArrowLeft, MdKeyboardArrowRight, MdKeyboardArrowDown } from "react-icons/md";
+import { GrSystem, GrEmptyCircle, } from "react-icons/gr";
 import { GiSwordsPower } from "react-icons/gi";
+import { FaSitemap } from "react-icons/fa";
+import { RiLightbulbFlashLine } from "react-icons/ri";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+
 
 const sidebarItems = [
   {
@@ -22,11 +27,31 @@ const sidebarItems = [
     href: "/",
     icon: AiOutlineHome,
   },
+
+  // {
+  //   name: "Our Value System",
+  //   href: "/OurValueSystem",
+  //   icon: GrSystem,
+  // },
+
   {
     name: "Our Commitment",
     href: "/OurServiceCharter",
     icon: GiSwordsPower,
   },
+
+  // {
+  //   name: "What's New",
+  //   href: "/WhatsNew",
+  //   icon: RiLightbulbFlashLine,
+  // },
+
+
+  // {
+  //   name: "Vacancies",
+  //   href: "/Vacancies",
+  //   icon: GrEmptyCircle,
+  // },
   {
     name: "International Ombudsman Institute",
     href: "/InternationalOmbudsman",
@@ -38,7 +63,7 @@ const sidebarItems = [
     icon: AiOutlineProject,
   },
   {
-    name: "Links",
+    name: "Links",  
     href: "/Links",
     icon: BsLink45Deg,
   },
@@ -47,33 +72,25 @@ const sidebarItems = [
     href: "/PhotoGallery",
     icon: BiPhotoAlbum,
   },
+
   {
     name: "Contact Us",
     href: "/contact",
     icon: TiContacts,
   },
-  {
-    name: "About",
-    icon: BsSkipStartCircle,
-    dropdown: [
-      { name: "Our Value System", href: "/OurValueSystem" },
-      { name: "What's New", href: "/WhatsNew" },
-      { name: "Vacancies", href: "/Vacancies" },
-      { name: "Decisions", href: "/Decisions" }, // Added "Decisions" under "About"
-    ],
-  },
+  // {
+  //   name: "Site Map",
+  //   href: "/SiteMap",
+  //   icon: FaSitemap,
+  // },
 ];
 
 const Sidebar = () => {
   const router = useRouter();
   const { isCollapsed, toggleSidebarcollapse } = useContext(SidebarContext);
-  const [aboutDropdownOpen, setAboutDropdownOpen] = useState(false);
-
-  const toggleAboutDropdown = () => {
-    setAboutDropdownOpen(!aboutDropdownOpen);
-  };
 
   return (
+
     <div className="sidebar__wrapper">
       <button className="sidebar-button" onClick={toggleSidebarcollapse}>
         {isCollapsed ? <MdKeyboardArrowRight /> : <MdKeyboardArrowLeft />}
@@ -90,46 +107,17 @@ const Sidebar = () => {
             />
           </Link>
           <div className="d-flex flex-column ms-3">
-            <p className="sidebar__logo-name">Provincial Ombudsman</p>
+            <p className="sidebar__logo-name">Provincial Ombudsman </p>
             <span>(Mohtasib) Sindh</span>
           </div>
         </div>
         <ul className="sidebar__list">
-          {sidebarItems.map(({ name, href, icon: Icon, dropdown }) => {
-            return dropdown ? (
-              <li className="sidebar__item" key={name}>
-                <div
-                  className="sidebar__link d-flex align-items-center"
-                  onClick={toggleAboutDropdown}
-                  style={{ cursor: "pointer" }}
-                >
-                  <span className="sidebar__icon">
-                    <Icon />
-                  </span>
-                  <span className="sidebar__name">{name}</span>
-                  <MdKeyboardArrowDown
-                    className={`ms-auto ${aboutDropdownOpen ? "rotate-180" : ""}`}
-                  />
-                </div>
-                {aboutDropdownOpen && (
-                  <ul className="sidebar__dropdown ms-4">
-                    {dropdown.map((item) => (
-                      <li key={item.name} className="sidebar__dropdown-item">
-                        <Link
-                          className={`sidebar__link ${router.pathname === item.href ? "sidebar__link--active" : ""}`}
-                          href={item.href}
-                        >
-                          <span className="sidebar__name">{item.name}</span>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </li>
-            ) : (
+          {sidebarItems.map(({ name, href, icon: Icon }) => {
+            return (
               <li className="sidebar__item" key={name}>
                 <Link
-                  className={`sidebar__link ${router.pathname === href ? "sidebar__link--active" : ""}`}
+                  className={`sidebar__link ${router.pathname === href ? "sidebar__link--active" : ""
+                    }`}
                   href={href}
                 >
                   <span className="sidebar__icon">
@@ -143,6 +131,7 @@ const Sidebar = () => {
         </ul>
       </aside>
     </div>
+
   );
 };
 
