58ea1853553fcbbfe94960423be0bc15e99d89a0
Tariq <tariq@wholydigital.com.au>
2025-01-14 23:25:37 +0500

    logs check

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,6 +40,7 @@ import {
 import { FaPlaneArrival, FaGlobeAmericas, FaHandshake } from "react-icons/fa";
 import { GiNotebook } from "react-icons/gi";
 
+//banners
 import banner1 from "../../public/banner-1.jpg";
 import banner2 from "../../public/banner-2.jpg";
 import banner3 from "../../public/banner-3.jpg";
